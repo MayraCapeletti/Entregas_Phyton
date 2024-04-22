658ddd2e68fd50fa5b9d722588653d24d527ecf9
MayraCapeletti <mayracapeletti@hotmail.com>
2024-04-22 20:48:58 -0300

    saque espacios

--- primera/primera_entrega.py
+++ primera/primera_entrega.py
@@ -2,13 +2,10 @@ usuarios = {}
 
 
 def registrar_usuario():
-
     usuario = input("Ingrese su nombre de usuario: ")
     contraseña = input("Ingrese su contraseña: ")
-
     if usuario in usuarios:
         print("El usuario ya existe, ingrese otro")
-
     else:
         usuarios[usuario] = contraseña
         print("Usuario ingresado correctamente")
@@ -16,7 +13,6 @@ def registrar_usuario():
 
 def mostrar_datos():
     print("Usarios registrados")
-
     for usuario, contraseña in usuarios.items():
         print(f"Usuario: {usuario} Contraseña: {contraseña}")
 
@@ -24,10 +20,8 @@ def mostrar_datos():
 def login_usuario():
     intentos = 0
     while intentos < 3:
-
         usuario = input("Ingrese su usario para ingresar: ")
         contraseña = input("Ingrese su contraseña para ingresar: ")
-
         if usuario in usuarios and usuarios[usuario] == contraseña:
             print("Bienvenido!")
             break
@@ -36,19 +30,16 @@ def login_usuario():
             if intentos < 3:
                 print("Usuario o contraseña Incorrectos")
                 print(f"Intento {intentos} de 3")
-
             else:
                 print("Usuario o Contraseña incorrectos. Se han terminados los intentos")
 
 
 while (True):
-
     print("\nOpciones: ")
     print("1. Registrar un nuevo usuario")
     print("2. Mostrar usuarios registrados")
     print("3. Login de usuario")
     print("4. Salir")
-
     opciones = input("Ingrese una opción: ")
 
     if opciones == "1":
