d5833d6c581513d66e73990c5674445e2071b1f7
Mayra <151087521+MayraCapeletti@users.noreply.github.com>
2024-06-21 20:29:57 -0300

    Delete tercera_pre_entrega directory

--- tercera_pre_entrega/Blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Autor, Categoria
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Autor)
-admin.site.register(Categoria)
